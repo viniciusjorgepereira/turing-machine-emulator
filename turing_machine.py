5bd335fb3d0ab03d8b1c718f41ef32b1c7141660
Vinicius Jorge Pereira <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-03-02 13:12:51 -0300

    Update turing_machine.py

--- turing_machine.py
+++ turing_machine.py
@@ -1,7 +1,8 @@
 from transfers import Transfers
 
 class TuringMachine():
-
+    
+    # Constructor
     def __init__(self, word, initial, acceptance, rejection):
         self.__type = Type(word)
         self.__states = Transfers(State(initial), State(acceptance), State(rejection))
