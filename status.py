79e74d1d2ff983900d071490b1d8c7daabf526d5
Vinicius Jorge Pereira <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-03-02 13:14:01 -0300

    Update status.py

--- status.py
+++ status.py
@@ -1,16 +1,20 @@
 class Status():
 
+    # Constructor
     def __init__(self, name):
         self.__name = name
         self.__status = {}
 
+    # Get name
     @property
     def name(self):
         return self.__name
 
+    # Get status
     @property
     def status(self):
         return self.__status
 
+    # Add in the dicionary
     def add(self, letter, status):
         self.status[letter] = status
