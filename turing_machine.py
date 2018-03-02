7aa69d272c5256cafcda44ae4a5ce6f2efaf3418
Vinicius Jorge Pereira <vinicius.jorge.silva@ccc.ufcg.edu.br>
2018-03-02 13:36:16 -0300

    correcting imports

--- turing_machine.py
+++ turing_machine.py
@@ -1,8 +1,10 @@
+from tape import Tape
+from status import Status
 from transfers import Transfers
 
 class TuringMachine():
     
     # Constructor
     def __init__(self, word, initial, acceptance, rejection):
-        self.__type = Type(word)
-        self.__states = Transfers(State(initial), State(acceptance), State(rejection))
+        self.__tape = Tape(word)
+        self.__status = Transfers(Status(initial), Status(acceptance), Status(rejection))
